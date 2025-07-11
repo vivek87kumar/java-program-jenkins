cde6539b8da767100105144c2c47d60dc2c4c455
Vivek Kumar <vivekumar97vk@gmail.com>
2025-07-11 09:18:08 +0530

    added again

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
         System.out.println("This is another commit");
+
+        Student student = new Student(); // ✅ this line is fine
+        student.sayHello(); 
+
     }
 
-    Student student = new Student();
-    student.sayHello();
+    
 }
--- Student.java
+++ Student.java
@@ -1,8 +1,9 @@
-public class Student{
-    public Student(){
+public class Student {
+    public Student() {
         System.out.println("Creating student object!");
     }
-    public void sayHello(){
+
+    public void sayHello() {
         System.out.println("Hi, I am Student");
     }
 }
